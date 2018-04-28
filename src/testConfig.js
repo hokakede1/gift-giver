d283eda7716c7db96cbe07671039633d8d7ee063
Huy Dang <hqdang97@gmail.com>
2018-04-28 14:58:12 -0600

    Initial commit

---
+++ src/testConfig.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
