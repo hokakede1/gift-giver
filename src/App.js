58a1a9e939d161eb18f19e2053621cb4c7e498b1
Huy Dang <hqdang97@gmail.com>
2018-04-29 12:22:44 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  state = { gifts: []}
+
+  addGift = () => {
+    const { gifts } = this.state;
+    const ids = this.state.gifts.map(gift => gift.id);
+    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({ id: max_id + 1})
+
+    this.setState({ gifts })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Gift Giver</h2>
+        <div className="gift-list">
+          {
+            this.state.gifts.map((gift, index) => {
+              return <div key={gift.id}></div>
+            })
+          }
+        </div>
+        <Button className="btn-add" onClick={this.addGift}> Add Gift </Button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,24 @@ import App from './App';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
+const app = shallow(<App/>)
 
 it('renders without crashing', () => {
-  const app = shallow(<App/>)
-  expect(app).toMatchSnapShot()
+  expect(app).toMatchSnapshot()
 });
+
+it('init the `state` with an empty list of gitfs', () => {
+  expect(app.state().gifts).toEqual([]);
+})
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+
+  expect(app.state().gifts).toEqual([{ id: 1}])
+})
+
+it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+
+  expect(app.find('.gift-list').children().length).toEqual(2);
+})
